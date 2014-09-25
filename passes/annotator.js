5a9856a240773b710271dfb49739625fea7c76f6
Filip Sobczak <filsob@gmail.com>
2014-09-25 13:32:53 +0200

    pass & signatures for ng

---
+++ passes/annotator.js
@@ -0,0 +1,34 @@
+// annotate "ngModule"
+
+var annotateInjectable = require('../lib/annotate-injectable');
+var annotatorPass = module.exports = require('astral-pass')();
+
+var deepCompare = require('../lib/deep-compare');
+
+
+annotatorPass.name = 'angular:annotator';
+annotatorPass.prereqs = [
+  'angular:annotator:mark'
+];
+
+annotatorPass.
+  when(require('../signatures/simple')).
+  do(function (chunk, info) {
+    var type;
+
+    if (chunk.callee &&
+      chunk.callee.property &&
+      chunk.callee.property.name) {
+      type = chunk.callee.property.name;
+    }
+    var argIndex = 1;
+    if (type === 'config' || type === 'run') {
+      argIndex = 0;
+    }
+
+    if (type === 'constant' || type === 'value') {
+      return;
+    }
+
+    annotateInjectable(chunk.arguments[0].value, chunk.arguments[argIndex]);
+  });
---
+++ passes/route.js
@@ -0,0 +1,78 @@
+// mark route objects
+
+var annotateInjectable = require('../lib/annotate-injectable');
+var deepApply = require('../lib/deep-apply');
+
+var routeAnnotatorPass = module.exports = {};
+routeAnnotatorPass.name = 'angular:annotator:route';
+routeAnnotatorPass.prereqs = [
+  'angular:annotator:mark'
+];
+
+routeAnnotatorPass.run = function (ast, info) {
+  deepApply(ast, [{
+    "type": "CallExpression",
+    "callee": {
+      "type": "MemberExpression",
+      "object": {
+        "ngModule": true
+      },
+      "property": {
+        "type": "Identifier",
+        "name": "config"
+      }
+    }
+  }], function (routeChunk) {
+    deepApply(routeChunk, [{
+      "type": "CallExpression",
+      "callee": {
+        "type": "MemberExpression",
+        "property": {
+          "type": "Identifier",
+          "name": "when"
+        }
+      },
+      "arguments": [ {},
+        {
+          "type": "ObjectExpression"
+        }
+      ]
+    }],function (whenChunk) {
+      deepApply(whenChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "controller"
+        },
+        "value": {
+          "type": "FunctionExpression"
+        }
+      }], function (controllerChunk) {
+        controllerChunk.value = annotateInjectable('controller route', controllerChunk.value);
+      });
+
+      deepApply(whenChunk, [{
+        "type": "Property",
+        "key": {
+          "type": "Identifier",
+          "name": "resolve"
+        },
+        "value": {
+          "type": "ObjectExpression"
+        }
+      }], function (resolveChunk) {
+        deepApply(resolveChunk, [{
+          "type": "Property",
+          "key": {
+            "type": "Identifier"
+          },
+          "value": {
+            "type": "FunctionExpression"
+          }
+        }], function (resolveIdentifierChunk) {
+          resolveIdentifierChunk.value = annotateInjectable(resolveIdentifierChunk.value);
+        });
+      });
+    });
+  });
+};

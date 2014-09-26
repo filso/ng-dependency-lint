07dca2e59c9e87ed585297f3aef1b70ade35905c
Filip Sobczak <filsob@gmail.com>
2014-09-26 14:46:31 +0200

    dev cycle script

---
+++ dev.js
@@ -0,0 +1,32 @@
+var assert = require('should');
+
+// so we don't have to put the stuff we're testing into a string
+var stringifyFunctionBody = require('./test/util').stringifyFunctionBody;
+var annotate = function(arg) {
+  return require('./main').lint(
+    stringifyFunctionBody(arg));
+};
+
+
+var annotated = annotate(function() {
+  angular.module('myMod', []).
+  directive('myDir', function() {
+    return {
+      controller: function($scope, bleble, fs) {
+        $scope.test = true;
+        !testVar;
+        b / bleble + a;
+        a+testVar;
+        a-testVar;
+        a=testVar;
+        a*testVar;
+        a/testVar;
+        a<testVar;
+        a>testVar;
+        a&testVar;
+
+
+      }
+    };
+  });
+});
